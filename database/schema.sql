72839fb461f67b2ae4184d2d2bb19a3ec54cac2c
Anusree M <anusree@sreeanusree192@gmail.com>
2026-02-08 16:04:27 +0530

    Added database schema

---
+++ database/schema.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE employee_db;
+USE employee_db;
+
+CREATE TABLE employee (
+    emp_id INT PRIMARY KEY,
+    name VARCHAR(50),
+    dob DATE,
+    address VARCHAR(100),
+    email VARCHAR(50),
+    phone VARCHAR(15),
+    designation VARCHAR(30),
+    department VARCHAR(30),
+    salary DOUBLE,
+    qualification VARCHAR(30)
+);
